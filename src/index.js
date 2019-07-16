7f829cbd8efb36d68424731c8c8fd5b8f8eeeb1c
jaojeda <j.a.ojeda@live.com>
2019-07-16 10:35:33 -0700

    got function to work and added questions for quiz

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import isYes from '../src/isYes.js';
+
+const quizButton = document.getElementById('quiz-button');
+const result = document.getElementById('quiz-result');
+
+
+quizButton.onclick = function() {
+    // const emptyString = '';
+
+    let name = '';
+    
+    const firstName = prompt('What\'s your first name?');
+    name = name + firstName;
+    
+    const lastName = prompt('What\'s your last name?');
+    name = name + ' ' + lastName;
+
+    const confirmed = confirm('Are you sure you want to take the quiz ' + firstName + '?');
+    if(confirmed === false) {
+        return;
+    }
+
+    let correctAnswers = 0;
+
+    const questionOne = prompt('Do I have dogs (Y/N)?');
+
+    const correctQuestionOne = true;
+    if(isYes(questionOne) === correctQuestionOne) {
+        correctAnswers += 1;
+    }
+    
+    const questionTwo = prompt('Do I paint (Y/N)?');
+
+    const correctQuestionTwo = true;
+    if(isYes(questionTwo) === correctQuestionTwo) {
+        correctAnswers += 1;
+    }
+
+    const questionThree = prompt('Am I a Portland native (Y/N)?');
+
+    const correctQuestionThree = false;
+    if(isYes(questionThree) === correctQuestionThree) {
+        correctAnswers += 1;
+    }
+
+    alert('See your restults!');
+
+    let response = 'Okay ' + name + ', you got ' + correctAnswers + '/3 questions right.';
+
+    result.textContent = response;
+};  
