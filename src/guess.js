d516453f4916fb9b54f9dbf593214eb2cfc9e767
jaojeda <j.a.ojeda@live.com>
2019-07-17 12:17:28 -0700

    got the random generator to work

--- src/guess.js
+++ src/guess.js
@@ -7,6 +7,10 @@ const winLose = document.getElementById('answer-message');
 
 let tries = 4;
 
+let correctNumber = Math.floor(Math.random() * 20) + 1;
+
+console.log(correctNumber);
+
 submitButton.addEventListener('click', () => {
     
     let guess = parseInt(userInput.value);
@@ -16,15 +20,15 @@ submitButton.addEventListener('click', () => {
     let triesMessage = 'You have ' + tries + ' left.';
     let answerMessage = '';
  
-    if((compareNumbers(guess, 4) === 1) && (tries > 0)) {
+    if((compareNumbers(guess, correctNumber) === 1) && (tries > 0)) {
         answerMessage = 'You guessed too high! Try again.';
     }
     
-    else if((compareNumbers(guess, 4) === -1) && (tries > 0)) {
+    else if((compareNumbers(guess, correctNumber) === -1) && (tries > 0)) {
         answerMessage = 'You guessed low! Try again';
     }
 
-    else if((compareNumbers(guess, 4) === 0) && (tries > 0)) {
+    else if((compareNumbers(guess, correctNumber) === 0) && (tries > 0)) {
         submitButton.disabled = true;
         triesMessage = 'You got it with ' + tries + ' tries left.';
         answerMessage = 'You guessed right! Great job!';
