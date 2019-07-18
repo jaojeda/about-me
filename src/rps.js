4db346da8a79377ac73de3c152581df39e929ba4
Sam Jespersen <heyheysamj@gmail.com>
2019-07-18 16:35:07 -0700

    added counter feature, verified functionality

--- src/rps.js
+++ src/rps.js
@@ -4,6 +4,11 @@ import { didPlayerWin } from './get-throw.js';
 const throwButton = document.getElementById('throw-button');
 const opHand = document.getElementById('hand');
 const message = document.getElementById('message');
+const winCountDisplay = document.getElementById('win-count');
+const lossCountDisplay = document.getElementById('loss-count');
+
+let winCounter = 0;
+let lossCounter = 0;
 
 throwButton.addEventListener('click', () => {
     const opThrow = getThrow();
@@ -20,15 +25,18 @@ throwButton.addEventListener('click', () => {
     const draw = opThrow === userThrow;
 
     const win = didPlayerWin(userThrow, opThrow);
-    
+
     if(draw) {
         message.textContent = 'Draw! Try again!';
     }
     else if(win) {
         message.textContent = 'Got me!';
-    }
-    else {
+        winCounter++;
+        winCountDisplay.textContent = winCounter;
+    } else {
         message.textContent = 'Sorry, wanna try again?';
+        lossCounter++;
+        lossCountDisplay.textContent = lossCounter;
     }
 });
 
