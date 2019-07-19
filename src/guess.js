f3511b5fffaf8f3ed546fab723fc4d189cfa6214
Sam Jespersen <heyheysamj@gmail.com>
2019-07-18 17:21:04 -0700

    fixed correct last guess bug

--- src/guess.js
+++ src/guess.js
@@ -8,36 +8,44 @@ const winLose = document.getElementById('answer-message');
 let tries = 4;
 
 let correctNumber = Math.floor(Math.random() * 20) + 1;
-
+console.log(correctNumber);
 
 submitButton.addEventListener('click', () => {
-    
+
     let guess = parseInt(userInput.value);
-    
+
     tries -= 1;
-    
+
     let triesMessage = 'You have ' + tries + ' left.';
     let answerMessage = '';
- 
-    if((compareNumbers(guess, correctNumber) === 1) && (tries > 0)) {
-        answerMessage = 'You guessed too high! Try again.';
-    }
-    
-    else if((compareNumbers(guess, correctNumber) === -1) && (tries > 0)) {
-        answerMessage = 'You guessed low! Try again';
-    }
 
-    else if((compareNumbers(guess, correctNumber) === 0) && (tries > 0)) {
+    if((compareNumbers(guess, correctNumber) === 0)) {
         submitButton.disabled = true;
         triesMessage = 'You got it with ' + tries + ' tries left.';
         answerMessage = 'You guessed right! Great job!';
+        resultMessage(triesMessage, answerMessage);
+        return;
     }
+    if(tries !== 0) {
+        if((compareNumbers(guess, correctNumber) === 1)) {
+            answerMessage = 'You guessed too high! Try again.';
+        }
 
-    else {
+        else if((compareNumbers(guess, correctNumber) === -1)) {
+            answerMessage = 'You guessed low! Try again';
+        }
+    } else {
+
         submitButton.disabled = true;
         answerMessage = 'Sorry, you are all out of tries.';
     }
-    triesDisplay.textContent = triesMessage;
-    winLose.textContent = answerMessage;
+
+
+    resultMessage(triesMessage, answerMessage);
 
 });
+
+function resultMessage(triesMessage, answerMessage) {
+    triesDisplay.textContent = triesMessage;
+    winLose.textContent = answerMessage;
+}
