a87b8f096137ce7c7504e40a7684c4c2aa1f0b1d
Sam Jespersen <heyheysamj@gmail.com>
2019-07-18 17:47:13 -0700

    changed title, fixed out-of-bounds number problem, refactored function into startgame

--- src/guess.js
+++ src/guess.js
@@ -8,44 +8,52 @@ const winLose = document.getElementById('answer-message');
 let tries = 4;
 
 let correctNumber = Math.floor(Math.random() * 20) + 1;
-console.log(correctNumber);
 
-submitButton.addEventListener('click', () => {
+submitButton.addEventListener('click', startGame);
 
-    let guess = parseInt(userInput.value);
 
-    tries -= 1;
+function startGame(){
 
-    let triesMessage = 'You have ' + tries + ' left.';
-    let answerMessage = '';
-
-    if((compareNumbers(guess, correctNumber) === 0)) {
-        submitButton.disabled = true;
-        triesMessage = 'You got it with ' + tries + ' tries left.';
-        answerMessage = 'You guessed right! Great job!';
-        resultMessage(triesMessage, answerMessage);
+    
+    if(userInput.value < 1 || userInput.value > 20){
+        alert("That's not a number between 1 and 20!");
         return;
-    }
-    if(tries !== 0) {
-        if((compareNumbers(guess, correctNumber) === 1)) {
-            answerMessage = 'You guessed too high! Try again.';
-        }
-
-        else if((compareNumbers(guess, correctNumber) === -1)) {
-            answerMessage = 'You guessed low! Try again';
-        }
     } else {
 
-        submitButton.disabled = true;
-        answerMessage = 'Sorry, you are all out of tries.';
+        let guess = parseInt(userInput.value);
+
+        tries -= 1;
+
+        let triesMessage = 'You have ' + tries + ' left.';
+        let answerMessage = '';
+
+        if((compareNumbers(guess, correctNumber) === 0)) {
+            submitButton.disabled = true;
+            triesMessage = 'You got it with ' + tries + ' tries left.';
+            answerMessage = 'You guessed right! Great job!';
+            resultMessage(triesMessage, answerMessage);
+            return;
+        }
+        if(tries !== 0) {
+            if((compareNumbers(guess, correctNumber) === 1)) {
+                answerMessage = 'You guessed too high! Try again.';
+            }
+
+            else if((compareNumbers(guess, correctNumber) === -1)) {
+                answerMessage = 'You guessed low! Try again';
+            }
+        } else {
+
+            submitButton.disabled = true;
+            answerMessage = 'Sorry, you are all out of tries.';
+        }
+
+
+        resultMessage(triesMessage, answerMessage);
     }
-
-
-    resultMessage(triesMessage, answerMessage);
-
-});
-
+}
 function resultMessage(triesMessage, answerMessage) {
     triesDisplay.textContent = triesMessage;
     winLose.textContent = answerMessage;
 }
+
